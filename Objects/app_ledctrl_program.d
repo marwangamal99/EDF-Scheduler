caf13d6db39e03ddc4c61d37244cc914324d5f20
Marwan Gamal Ahmed Abdalazzim Youssef 1806094 <1806094@eng.asu.edu.eg>
2022-09-17 00:08:27 +0200

    First commit

---
+++ Objects/app_ledctrl_program.d
@@ -0,0 +1,7 @@
+./objects/app_ledctrl_program.o: APP\Led_Ctrl\App_LedCtrl_Program.c \
+  APP\Led_Ctrl\App_LedCtrl_Interface.h COMMON\Std_Types.h \
+  COMMON\Platform_Types.h COMMON\Compiler.h HAL\LED\Led_Interface.h \
+  MCAL\1.\ GPIO\Gpio_Interface.h MCAL\1.\ GPIO\Gpio_Config.h \
+  MCAL\3.\ GPT\Gpt_Interface.h MCAL\3.\ GPT\Gpt_Config.h \
+  MCAL\3.\ GPT\Gpt_Private.h MCAL\2.\ NVIC\Nvic_Interface.h \
+  MCAL\2.\ NVIC\Nvic_Config.h
---
+++ Objects/main.d
@@ -0,0 +1,2 @@
+./objects/main.o: APP\main.c APP\Led_Ctrl\App_LedCtrl_Interface.h \
+  COMMON\Std_Types.h COMMON\Platform_Types.h COMMON\Compiler.h
